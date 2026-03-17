a8576fde8d6c86f64b15af0f0a109c30e7146ac6
GarryChen-site <chenymj23@gmail.com>
2026-03-17 19:30:19 +0900

    feat: Add `decltype` feature examples demonstrating its various uses and integrate them into the CMake build system.

---
+++ decltype/main.cc
@@ -0,0 +1,51 @@
+#include <cassert>
+#include <iostream>
+#include <type_traits>
+
+struct A {
+    double x;
+};
+const A* a;
+
+decltype(a->x) y;
+decltype((a->x)) z = y;
+
+template <typename T, typename U>
+auto add(T t, U u) -> decltype(t + u) {
+    return t + u;
+}
+
+const int& getRef(const int* p) { return *p; }
+
+static_assert(std::is_same_v<decltype(getRef), const int&(const int*)>);
+
+auto getRefFwdBad(const int* p) { return getRef(p); }
+
+static_assert(std::is_same_v<decltype(getRefFwdBad), int(const int*)>,
+              "Just returing auto isn't perfect forwarding.");
+
+decltype(auto) getRefFwdGood(const int* p) { return getRef(p); }
+
+static_assert(std::is_same_v<decltype(getRefFwdGood), const int&(const int*)>,
+              "Returning decltype(auto) perfectly forwards the return type.");
+
+auto getRefFwdGood1(const int* p) -> decltype(getRef(p)) { return getRef(p); }
+
+static_assert(std::is_same_v<decltype(getRefFwdGood1), const int&(const int*)>,
+              "Returing decltype(return expression) also perfectly forwards "
+              "the return type.");
+
+int main() {
+    int i = 33;
+    decltype(i) j = i * 2;
+    static_assert(std::is_same_v<decltype(i), decltype(j)>);
+    assert(i == 33 && 66 == j);
+
+    auto f = [i](int av, int bv) -> int { return av * bv + i; };
+    auto h = [i](int av, int bv) -> int { return av * bv + i; };
+    static_assert(!std::is_same_v<decltype(f), decltype(h)>,
+                  "The type of a lambda function is unique and unnamed");
+
+    decltype(f) g = f;
+    std::cout << f(3, 3) << ' ' << g(3, 3) << std::endl;
+}
